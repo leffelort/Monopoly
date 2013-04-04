ab4b876002b1044ca52ad9ef44831ad2e4d63981
Tyler Hedrick <tylerhedrick@me.com>
2013-04-04 11:20:48 -0400

    adding an initial version of the inspect view and socketio for peter

--- js/propertyView.js
+++ js/propertyView.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
 	$(".propertyCell").click(function(){
 		$(".propertyCell.selected").removeClass("selected");
 		$(this).addClass("selected");
+		displayPropertyDetails(this);
 	});
 });
+
+function displayPropertyDetails(property) {
+	console.log(property);
+}
