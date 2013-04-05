1244b074e61ca6c851f27853a55533d851a2420a
Tyler Hedrick <tylerhedrick@me.com>
2013-04-05 16:54:08 -0400

    inspect.html is now completely dynamic from the database and shows up really well on a mobile device with the property card and stuff

--- app.js
+++ app.js
@@ -17,6 +17,61 @@ app.get("/:staticFilename", function (request, response) {
 });
 */
 
+app.get("/properties", function(req, resp) {
+	var userid = req.body.userid;
+	var gameid = req.body.gameid;
+	getPropertiesFromDatabase(function(obj){ 
+		console.log(obj);
+		resp.send({
+			props : obj,
+			success:true
+		});
+	});
+});
+
+var mongo = require('mongodb');
+var host = 'localhost';
+var port = mongo.Connection.DEFAULT_PORT;
+
+var options = {w : 1};
+var dbName = 'monopoly';
+
+var client = new mongo.Db(
+		dbName,
+		new mongo.Server(host, port),
+		options
+	);
+
+function getPropertiesFromDatabase(onOpen) {
+
+	client.open(onDbReady);
+
+	function onDbReady(error) {
+		if (error) 
+			throw error;
+		client.collection('properties', onPropertyCollectionReady);
+	}
+
+	function logger(error, result) {
+		if (error)
+			throw error;
+		console.log(result);
+	}
+
+	function onPropertyCollectionReady(error, propertyCollection) {
+		if (error)
+			throw error;
+		var props = propertyCollection.find({}).toArray(function(err, array) {
+			console.log(array);
+			onOpen(array);
+			closeDb();
+		});
+	}
+}
+
+function closeDb() {
+	client.close();
+}
 
 app.listen(11611);
 
@@ -29,6 +84,7 @@ app.listen(11611);
 var io = require('socket.io').listen(8686);
 
 io.sockets.on('connection', function (socket) {
- socket.on('code', function (data) {console.log(data)});
+  socket.on('code', function (data) {console.log(data)});
   socket.on('disconnect', function () { });
 });
+
