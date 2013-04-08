8e3234e20d5e12b042c01a31735553cd303cdbe8
Tyler Hedrick <tylerhedrick@me.com>
2013-04-08 00:13:58 -0400

    updating stuff to work with heroku and mongodb and what not. This better work or imma be piased

--- mongodb_collection_transfer.py
+++ mongodb_collection_transfer.py
@@ -1,18 +1,19 @@
 import pymongo
 
-client = pymongo.MongoClient('localhost', 27017)
+client = pymongo.MongoClient('ds037067.mongolab.com', 37067)
 db = client.monopoly
-
+db.authenticate("thedrick", "thedrick")
 collection = db.properties
 
-mongolab = pymongo.MongoClient('ds037067.mongolab.com', 37067)
-mongolab_monopoly = mongolab.monopoly
-mongolab_monopoly.authenticate("thedrick", "thedrick")
+mongolab = pymongo.MongoClient('ds045557.mongolab.com', 45557)
+mongolab_monopoly = mongolab["heroku_app14631401"]
+mongolab_monopoly.authenticate("heroku_app14631401", "4ccecvoit0u9pn6ssu7gmm439g")
 mongolab_props = mongolab_monopoly.properties
 
 props_to_insert = []
 
 for prop in collection.find():
+	print prop
 	props_to_insert.append(prop)
 
-## mongolab_props.insert(props_to_insert)
+#mongolab_props.insert(props_to_insert)
