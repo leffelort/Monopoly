91cdbd949cbbec0b5e46780f3ed8573fc3fd84d1
Peter J. Marino <pmarino@andrew.cmu.edu>
2013-04-08 16:04:31 -0400

    adding evan-code, hoping not to have conflicts.

--- app.js
+++ app.js
@@ -10,6 +10,58 @@ var phoneCodeGen = require("./phoneCodeGen.js");
 
 app.use(express.bodyParser());
 
+/* borrowed from Evan Shapiro's Case Study, from Class notes. */ 
+/*
+app.all('/', function mobileDesktopRouter(req, res, next){
+    if (!strStartsWith(req.url, '/desktop') && !strStartsWith(req.url, '/mobile')){
+        if (req.useragent.isMobile){
+            wwwExists('/mobile' + req.url, function(exists){
+                if (exists)
+                    req.url = '/mobile' + req.url;
+                next();
+            });
+        }
+        else {
+            wwwExists('static/desktop' + req.url, function(exists){
+                if (exists)
+                    req.url = '/desktop' + req.url;
+                next();
+            });
+        }
+    }
+    else {
+        next();
+    }
+});
+
+function wwwExists(url, done){
+    if (strEndsWith(url, '/'))
+        url += 'index.html';
+    fs.exists(url, done);
+}
+
+function strStartsWith(str, prefix) {
+    return str.indexOf(prefix) === 0;
+}
+
+function strEndsWith(str, suffix) {
+    return str.match(suffix + "$") == suffix;
+}
+*/
+/* end of borrowed code */
+
+app.get('/desktop/', function(req, res, next){
+    mongoExpressAuth.checkLogin(req, res, function(err){
+        if (err)
+            res.sendfile('static/login.html');
+        else
+            next();
+    });
+});
+
+
+
+
 app.configure(function(){
   app.use(express.static(__dirname));
 });
@@ -298,7 +350,7 @@ io.sockets.on('connection', function (socket) {
 	  }
 	}
   });
-
+  
   socket.on('disconnect', function () {
     delete connections[socket.id];
   });
