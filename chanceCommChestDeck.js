f7eb9ed3ad7ee690b235ddc68fbab18a98d1ad41
Andrew Mittereder <andrew.mittereder@gmail.com>
2013-04-23 01:03:53 -0400

    rawr fixes

--- chanceCommChestDeck.js
+++ chanceCommChestDeck.js
@@ -5,8 +5,52 @@ var client;
 
 module.exports = function init(c) {
   client = c;
+  
+  return {
+    drawChance: function(game) {
+      if (game.chanceDeck.length === 0) {
+        // If deck is empty, query the database again
+        client.collection('chance', function (error, coll) {
+          if (!error) {
+            coll.toArray(function (err, arr) {
+              if (!err) {
+                game.chanceDeck = arr;
+                if (game.chanceJailCardUsed) {
+                  game.chanceDeck.splice(12, 1);
+                }
+              }
+            });
+          }
+        });
+      }
+    },
+    drawCommChest: function(game) {
+      if (game.commChestDeck.length === 0) {
+        // If deck is empty, query the database again
+        client.collection('communitychest', function (error, coll) {
+          if (!error) {
+            coll.toArray(function (err, arr) {
+              if (!err) {
+                game.commChestDeck = arr;
+                if (game.commChestJailCardUsed) {
+                  game.commChestDeck.splice(12, 1);
+                }
+              }
+            });
+          }
+        });
+      }
+    },
+    returnChanceJailCard = function(game) {
+      game.chanceJailCardUsed = false;
+    },
+    returnCommChestJailCard = function(game) {
+      game.commChestJailCardUsed = false;
+    }
+  };
 }
 
+/*
 module.exports.drawChance = function(game) {
   if (game.chanceDeck.length === 0) {
     // If deck is empty, query the database again
@@ -75,3 +119,4 @@ module.exports.returnChanceJailCard = function(game) {
 module.exports.returnCommChestJailCard = function(game) {
   game.commChestJailCardUsed = false;
 }
+*/
